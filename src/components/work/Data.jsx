544bf2e7b9499a94d72456feb0f8297fd42cdb5b
Ritesh7459 <140267718+Ritesh7m@users.noreply.github.com>
2025-07-24 00:33:17 +0530

    Update Data.jsx

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -5,17 +5,6 @@ import Project2 from "../../assets/project2.png";
 export const Data = [
   {
     id: 1,
-    image: Project2,
-    title: "E-Learning Platform",
-    category: "Web Development",
-    description:
-      "Built a Udemy-like e-learning platform using MERN stack with secure authentication, video streaming, course management, and integrated payment gateway.",
-    codeLink: "",
-    demoLink: "", 
-  },
-
-  {
-    id: 3,
     image: Project1,
     title: "E-Commerce for RO Filters",
     category: "Web Development",
@@ -24,4 +13,15 @@ export const Data = [
     codeLink: "https://github.com/Ritesh7m/RS_enterprises",
     demoLink: "https://rs-enterprises-frontend.vercel.app/",
   },
+  {
+    id: 2,
+    image: Project2,
+    title: "E-Learning Platform",
+    category: "Web Development",
+    description:
+      "Built a Udemy-like e-learning platform using MERN stack with secure authentication, video streaming, course management, and integrated payment gateway.",
+    codeLink: "https://github.com/Ritesh7m/StudyNotion",
+    demoLink: "https://github.com/Ritesh7m/StudyNotion", 
+  },
+
 ];
