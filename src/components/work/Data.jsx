6a775a2edf71ef15b464ac0ad3ebcd6d1080ecb2
Ritesh Maurya <maurya.ritesh2005@gmail.com>
2026-03-10 02:48:44 +0530

    update resume

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -3,17 +3,7 @@ import Project2 from "../../assets/project2.png";
 import Project3 from "../../assets/project3.png";
 
 export const Data = [
-  {
-    id: 1,
-    image: Project1,
-    title: "E-Commerce Website",
-    category: "Web Development",
-    description:
-      "Built a MERN stack e-commerce website for RS Enterprises to sell water purifier filters online, featuring product listings, shopping cart, secure checkout, and smooth order management for customers and admins. ",
-    codeLink: "https://github.com/Ritesh7m/RS_enterprises",
-    demoLink: "https://rs-enterprises-frontend.vercel.app/",
-  },
-  {
+   {
     id: 2,
     image: Project2,
     title: "E-Learning Platform",
@@ -24,13 +14,24 @@ export const Data = [
     demoLink: "https://studywebapp.vercel.app/",
   },
   {
-    id: 3,
-    image: Project3,
-    title: "LeetStatus ",
+    id: 1,
+    image: Project1,
+    title: "E-Commerce Website",
     category: "Web Development",
     description:
-      "A React web app that lets users instantly check their LeetCode statistics. Enter a username to fetch and view profile details such as total problems solved, difficulty wise breakdown, and join date all in a simple and intuitive interface.",
-    codeLink: "https://github.com/Ritesh7m/leetcode-status",
-    demoLink: "https://leetstatus.vercel.app/",
+      "Built a MERN stack e-commerce website for RS Enterprises to sell water purifier filters online, featuring product listings, shopping cart, secure checkout, and smooth order management for customers and admins. ",
+    codeLink: "https://github.com/Ritesh7m/RS_enterprises",
+    demoLink: "https://rs-enterprises-frontend.vercel.app/",
+  },
+ 
+  {
+    id: 3,
+    image: Project3,
+    title: "QuickPaste",
+    category: "Web Development",
+    description:
+      "QuickPaste is a lightweight pastebin tool for storing and sharing text or code snippets instantly. Create a paste, toggle it public or private, and share it via a direct link  no account needed. Public pastes are visible to everyone, while private ones are only accessible to those with the link. Built with React, Redux, Node.js, and MongoDB.",
+    codeLink: "https://github.com/Ritesh7m/Quick-paste",
+    demoLink: "https://rpaste.vercel.app/",
   },
 ];
